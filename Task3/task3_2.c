f179053451eb99665853480cc5caab03b90161d4
THAPER  POONEET <thap6537@css2c70.engr.ccny.cuny.edu>
2018-03-16 12:37:01 -0400

    Fixed the null parameter missing for execvp

--- Task3/task3_2.c
+++ Task3/task3_2.c
@@ -4,13 +4,14 @@
 #include <stdio.h>
 
 int main(int argc, char* argv[]){
-  char *const child_args[] = {"ls", "-lat", "/usr/bin"};
+  char *const child_args[] = {"ls", "-lat", "/usr/bin", NULL};
   int child = fork();
   int status;
   if (child==0) {
     int pid = execvp("ls", child_args);
+    //int pid = execvp("ls", argv);
   } else {
-    printf("Child pid: %d", child);
+    printf("Child pid: %d\n", child);
     waitpid(child, &status, 0);
   }
   return 0;
