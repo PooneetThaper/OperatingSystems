8d9ed0d4f5ada64afe03301a40c516570378da55
Pooneet Thaper <pooneetthaper@gmail.com>
2018-03-18 22:36:07 -0400

    Linux Lab does not have pandas installed so the check is changed to use csv instead

--- Task4/check_averages.py
+++ Task4/check_averages.py
@@ -1,7 +1,22 @@
-import pandas as pd
+import csv
 import numpy as np
 
-df = pd.read_csv("scores.csv",header=None)
-vals = df.iloc[:,:-1].values
+vals = []
 
-print([sum(vals[:,i])/len(vals) for i in range(len(vals[0]))])
+with open("scores.csv","r") as csvfile:
+    reader = csv.reader(csvfile, delimiter=' ')
+    for row in reader:
+        vals.append([int(entry) for entry in row[0].split(',')[:-1]])
+
+vals = np.array(vals)
+averages = [sum(vals[:,i])/len(vals) for i in range(len(vals[0]))]
+for i in range(len(averages)):
+    print("Assignement {} Average: {}".format(i, averages[i]))
+
+# import pandas as pd
+# import numpy as np
+#
+# df = pd.read_csv("scores.csv",header=None)
+# vals = df.iloc[:,:-1].values
+#
+# print([sum(vals[:,i])/len(vals) for i in range(len(vals[0]))])
